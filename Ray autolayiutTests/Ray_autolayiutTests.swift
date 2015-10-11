359ac8cdc89ba81520130e8a6b5204e8394fd648
sathish chinniah <sathish73ec@gmail.com>
2015-10-11 19:55:03 +0530

    Initial Commit

---
+++ Ray autolayiutTests/Ray_autolayiutTests.swift
@@ -0,0 +1,36 @@
+//
+//  Ray_autolayiutTests.swift
+//  Ray autolayiutTests
+//
+//  Created by Sathish Chinniah on 11/10/15.
+//  Copyright © 2015 Sathish Chinniah. All rights reserved.
+//
+
+import XCTest
+@testable import Ray_autolayiut
+
+class Ray_autolayiutTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
